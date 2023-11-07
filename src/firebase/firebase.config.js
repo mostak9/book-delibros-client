d585869a26791183cf31b81e67d32203aafbd0e0
mostak9 <mostak.ahm263@gmail.com>
2023-11-07 23:44:37 +0600

    adde env file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import {getAuth} from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDI1Lx-9753ZkYJj7_9KGFqjYYj7mf0kTM",
-  authDomain: "library-management-d2da6.firebaseapp.com",
-  projectId: "library-management-d2da6",
-  storageBucket: "library-management-d2da6.appspot.com",
-  messagingSenderId: "841985577485",
-  appId: "1:841985577485:web:2c371900d41685589e7b44"
+  apiKey: import.meta.env.VITE_API_KEY ,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN ,
+  projectId: import.meta.env.VITE_PROJECT_ID ,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET ,
+  messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID ,
+  appId: import.meta.env.VITE_APP_ID ,
 };
 
 // Initialize Firebase
